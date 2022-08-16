1e050dbc490263487456d30d79b799262454efcd
harshsareen03 <63228890+harshsareen03@users.noreply.github.com>
2022-08-16 17:52:35 +0530

    Add files via upload

---
+++ components/HL.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet, Text, View,ImageBackground } from 'react-native';
+import Style from './Style'
+const HL=()=>{
+    return(
+    <View>
+        <Text>hello</Text>
+        <ImageBackground source='../images/ioj.jpg' style={Style.image}/>
+
+
+
+    
+    </View>
+
+    )
+
+}
+
+export default HL
